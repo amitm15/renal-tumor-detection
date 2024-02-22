b88286b6f5a212ab459e8c5c1805c8598b23cae0
amitm15 <amitmaity0415@gmail.com>
2024-02-22 22:30:31 +0530

    Base-model prepared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from diseaseClassfier import logger
 from diseaseClassfier.pipeline.stage0_data_ingestion import DataIngestionTrainingPipeline
-# from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from diseaseClassfier.pipeline.stage1_base_model import PrepareBaseModelTrainingPipeline
 # from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 # from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
@@ -10,6 +10,18 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Prepare Base Model"
+
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
